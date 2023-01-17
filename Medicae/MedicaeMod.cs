264a040b89ce83968ebc99ad5b1ca65291d61456
Frank Novoa <herzleiid@gmail.com>
2023-01-16 21:54:10 -0300

    added and assigned capi and sapi variables

--- Medicae/MedicaeMod.cs
+++ Medicae/MedicaeMod.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using Vintagestory.API;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Config;
@@ -14,12 +15,18 @@ namespace Medicae;
 public class Medicae : ModSystem
 {
     private ICoreAPI _api;
+    private ICoreClientAPI _capi;
+    private ICoreServerAPI _sapi;
 
     public override void Start(ICoreAPI api)
     {
         base.Start(api);
         this._api = api;
-
+        if (api.Side == EnumAppSide.Server)
+            _sapi = api as ICoreServerAPI;
+        else
+            _capi = api as ICoreClientAPI;
+        
         this._api.RegisterItemClass("ItemTourniquet", typeof(ItemTourniquet));
         this._api.RegisterItemClass("ItemSplint", typeof(ItemSplint));
 
